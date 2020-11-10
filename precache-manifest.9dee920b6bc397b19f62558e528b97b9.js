6deb1b05dd2bedfe2a00cbe815a8d59992aa3817
Almaz <atunstyle@gmail.com>
2020-11-11 01:14:16 +0300

    Updates

---
+++ precache-manifest.9dee920b6bc397b19f62558e528b97b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd816404bf7319f089d5a045614491e2",
+    "url": "/beauty-dnd/index.html"
+  },
+  {
+    "revision": "b10ba70cbed8e7b8865a",
+    "url": "/beauty-dnd/static/css/main.c109f5cd.chunk.css"
+  },
+  {
+    "revision": "a41656bf8712f1e587ee",
+    "url": "/beauty-dnd/static/js/2.b2eaac50.chunk.js"
+  },
+  {
+    "revision": "28f29a598e465553cfeea810767399d3",
+    "url": "/beauty-dnd/static/js/2.b2eaac50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10ba70cbed8e7b8865a",
+    "url": "/beauty-dnd/static/js/main.55343ba2.chunk.js"
+  },
+  {
+    "revision": "c39e59384e5f1e5b8831",
+    "url": "/beauty-dnd/static/js/runtime-main.7afa79f3.js"
+  }
+]);
